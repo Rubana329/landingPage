27385a3d9cf182f316a0581bf09f491d8bacbc03
Rubana329 <rubanaali265@gmail.com>
2025-05-31 19:34:04 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// Change navbar style on scroll
+window.addEventListener('scroll', () => {
+  const navbar = document.getElementById('navbar');
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
+
+// Optional: toggle nav on mobile (hamburger menu can be added later)
